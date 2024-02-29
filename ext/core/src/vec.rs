c4dea3bb58368b6a23f643801d36026f629fdbce
German Odilov <godilov@skiff.com>
2024-02-29 17:52:42 +0000

    fix(vec): better vec ops generation syntax

--- ext/core/src/vec.rs
+++ ext/core/src/vec.rs
@@ -20,7 +20,7 @@ macro_rules! vec_ops_impl {
 }
 
 macro_rules! vec_impl {
-    ($n:tt => [$($type:path),+]) => {
+    ($n:tt: [$($type:path),+]) => {
         $(
             ops_impl_bin!(|a: &$type, b: &$type| -> $type,
                     + { $type (vec_ops_impl!($n: (a.0) + (b.0) [0] -> [])) }
@@ -31,34 +31,34 @@ macro_rules! vec_impl {
     };
 }
 
-vec_impl!(1 => [Vec<u8, 1>, Vec<u16, 1>, Vec<u32, 1>, Vec<u64, 1>, Vec<u128, 1>]);
-vec_impl!(1 => [Vec<i8, 1>, Vec<i16, 1>, Vec<i32, 1>, Vec<i64, 1>, Vec<i128, 1>]);
-vec_impl!(1 => [Vec<f32, 1>, Vec<f64, 1>]);
+vec_impl!(1: [Vec<u8, 1>, Vec<u16, 1>, Vec<u32, 1>, Vec<u64, 1>, Vec<u128, 1>]);
+vec_impl!(1: [Vec<i8, 1>, Vec<i16, 1>, Vec<i32, 1>, Vec<i64, 1>, Vec<i128, 1>]);
+vec_impl!(1: [Vec<f32, 1>, Vec<f64, 1>]);
 
-vec_impl!(2 => [Vec<u8, 2>, Vec<u16, 2>, Vec<u32, 2>, Vec<u64, 2>, Vec<u128, 2>]);
-vec_impl!(2 => [Vec<i8, 2>, Vec<i16, 2>, Vec<i32, 2>, Vec<i64, 2>, Vec<i128, 2>]);
-vec_impl!(2 => [Vec<f32, 2>, Vec<f64, 2>]);
+vec_impl!(2: [Vec<u8, 2>, Vec<u16, 2>, Vec<u32, 2>, Vec<u64, 2>, Vec<u128, 2>]);
+vec_impl!(2: [Vec<i8, 2>, Vec<i16, 2>, Vec<i32, 2>, Vec<i64, 2>, Vec<i128, 2>]);
+vec_impl!(2: [Vec<f32, 2>, Vec<f64, 2>]);
 
-vec_impl!(3 => [Vec<u8, 3>, Vec<u16, 3>, Vec<u32, 3>, Vec<u64, 3>, Vec<u128, 3>]);
-vec_impl!(3 => [Vec<i8, 3>, Vec<i16, 3>, Vec<i32, 3>, Vec<i64, 3>, Vec<i128, 3>]);
-vec_impl!(3 => [Vec<f32, 3>, Vec<f64, 3>]);
+vec_impl!(3: [Vec<u8, 3>, Vec<u16, 3>, Vec<u32, 3>, Vec<u64, 3>, Vec<u128, 3>]);
+vec_impl!(3: [Vec<i8, 3>, Vec<i16, 3>, Vec<i32, 3>, Vec<i64, 3>, Vec<i128, 3>]);
+vec_impl!(3: [Vec<f32, 3>, Vec<f64, 3>]);
 
-vec_impl!(4 => [Vec<u8, 4>, Vec<u16, 4>, Vec<u32, 4>, Vec<u64, 4>, Vec<u128, 4>]);
-vec_impl!(4 => [Vec<i8, 4>, Vec<i16, 4>, Vec<i32, 4>, Vec<i64, 4>, Vec<i128, 4>]);
-vec_impl!(4 => [Vec<f32, 4>, Vec<f64, 4>]);
+vec_impl!(4: [Vec<u8, 4>, Vec<u16, 4>, Vec<u32, 4>, Vec<u64, 4>, Vec<u128, 4>]);
+vec_impl!(4: [Vec<i8, 4>, Vec<i16, 4>, Vec<i32, 4>, Vec<i64, 4>, Vec<i128, 4>]);
+vec_impl!(4: [Vec<f32, 4>, Vec<f64, 4>]);
 
-vec_impl!(5 => [Vec<u8, 5>, Vec<u16, 5>, Vec<u32, 5>, Vec<u64, 5>, Vec<u128, 5>]);
-vec_impl!(5 => [Vec<i8, 5>, Vec<i16, 5>, Vec<i32, 5>, Vec<i64, 5>, Vec<i128, 5>]);
-vec_impl!(5 => [Vec<f32, 5>, Vec<f64, 5>]);
+vec_impl!(5: [Vec<u8, 5>, Vec<u16, 5>, Vec<u32, 5>, Vec<u64, 5>, Vec<u128, 5>]);
+vec_impl!(5: [Vec<i8, 5>, Vec<i16, 5>, Vec<i32, 5>, Vec<i64, 5>, Vec<i128, 5>]);
+vec_impl!(5: [Vec<f32, 5>, Vec<f64, 5>]);
 
-vec_impl!(6 => [Vec<u8, 6>, Vec<u16, 6>, Vec<u32, 6>, Vec<u64, 6>, Vec<u128, 6>]);
-vec_impl!(6 => [Vec<i8, 6>, Vec<i16, 6>, Vec<i32, 6>, Vec<i64, 6>, Vec<i128, 6>]);
-vec_impl!(6 => [Vec<f32, 6>, Vec<f64, 6>]);
+vec_impl!(6: [Vec<u8, 6>, Vec<u16, 6>, Vec<u32, 6>, Vec<u64, 6>, Vec<u128, 6>]);
+vec_impl!(6: [Vec<i8, 6>, Vec<i16, 6>, Vec<i32, 6>, Vec<i64, 6>, Vec<i128, 6>]);
+vec_impl!(6: [Vec<f32, 6>, Vec<f64, 6>]);
 
-vec_impl!(7 => [Vec<u8, 7>, Vec<u16, 7>, Vec<u32, 7>, Vec<u64, 7>, Vec<u128, 7>]);
-vec_impl!(7 => [Vec<i8, 7>, Vec<i16, 7>, Vec<i32, 7>, Vec<i64, 7>, Vec<i128, 7>]);
-vec_impl!(7 => [Vec<f32, 7>, Vec<f64, 7>]);
+vec_impl!(7: [Vec<u8, 7>, Vec<u16, 7>, Vec<u32, 7>, Vec<u64, 7>, Vec<u128, 7>]);
+vec_impl!(7: [Vec<i8, 7>, Vec<i16, 7>, Vec<i32, 7>, Vec<i64, 7>, Vec<i128, 7>]);
+vec_impl!(7: [Vec<f32, 7>, Vec<f64, 7>]);
 
-vec_impl!(8 => [Vec<u8, 8>, Vec<u16, 8>, Vec<u32, 8>, Vec<u64, 8>, Vec<u128, 8>]);
-vec_impl!(8 => [Vec<i8, 8>, Vec<i16, 8>, Vec<i32, 8>, Vec<i64, 8>, Vec<i128, 8>]);
-vec_impl!(8 => [Vec<f32, 8>, Vec<f64, 8>]);
+vec_impl!(8: [Vec<u8, 8>, Vec<u16, 8>, Vec<u32, 8>, Vec<u64, 8>, Vec<u128, 8>]);
+vec_impl!(8: [Vec<i8, 8>, Vec<i16, 8>, Vec<i32, 8>, Vec<i64, 8>, Vec<i128, 8>]);
+vec_impl!(8: [Vec<f32, 8>, Vec<f64, 8>]);
