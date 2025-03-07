5aa0514147455806a252677105285dbc95cea4ac
German Odilov <godilov@tuta.io>
2025-03-07 19:40:13 +0500

    feat(math): prime iterator

--- src/math.rs
+++ src/math.rs
@@ -1,30 +1,60 @@
-// use crate::ops::OpsRem;
-//
-// pub fn gcd<X: OpsRem<Output = X> + PartialEq + Eq>(
-//     a: X,
-//     b: X,
-// ) -> X {
-//     if b != 0 {
-//         gcd(b, a % b)
-//     } else {
-//         a
-//     }
-// }
-//
-// fn _gcd(
-//     a: u64,
-//     b: u64,
-// ) -> u64 {
-//     if b != 0 {
-//         gcd(b, a % b)
-//     } else {
-//         a
-//     }
-// }
-//
-// fn _lcm(
-//     a: u64,
-//     b: u64,
-// ) -> u64 {
-//     a / gcd(a, b) * b
-// }
+pub type Prime = u64;
+
+pub struct Primes {}
+
+struct PrimesIter {
+    data: Vec<Prime>,
+    next: Prime,
+    idx: usize,
+    len: usize,
+}
+
+impl Iterator for PrimesIter {
+    type Item = Prime;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.len == self.idx {
+            return None;
+        }
+
+        self.data.push(self.next);
+
+        for n in self.next + 1..Prime::MAX {
+            let mut is_prime = true;
+            let mut idx = 0;
+
+            while is_prime && self.data[idx] * self.data[idx] <= n {
+                is_prime &= (n % self.data[idx]) != 0;
+                idx += 1;
+            }
+
+            if is_prime {
+                self.next = n;
+                self.idx += 1;
+
+                break;
+            }
+        }
+
+        self.data.last().copied()
+    }
+}
+
+impl Primes {
+    pub fn iter(count: usize) -> impl Iterator<Item = Prime> {
+        PrimesIter {
+            data: Vec::with_capacity(count),
+            next: 2,
+            idx: 0,
+            len: count,
+        }
+    }
+}
+
+pub fn gcd() {
+    todo!()
+}
+
+pub fn lcm() {
+    todo!()
+}
