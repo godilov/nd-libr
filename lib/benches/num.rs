f44e5bc9967f9e2440a24ba8f6de6d56bc777753
German Odilov <godilov@tuta.io>
2025-05-17 02:55:36 +0500

    refactor(num): better benches

--- lib/benches/num.rs
+++ lib/benches/num.rs
@@ -1,5 +1,9 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use ndlib::{num::*, signed_fixed, unsigned_fixed};
+use ndlib::{
+    num::*,
+    ops::{Ops, OpsFrom},
+    signed_fixed, unsigned_fixed,
+};
 
 const PRIMES: [u64; 5] = [
     29_996_224_275_833,
@@ -12,34 +16,46 @@ const PRIMES: [u64; 5] = [
 type S64 = signed_fixed!(64);
 type U64 = unsigned_fixed!(64);
 
+macro_rules! composite {
+    ($type:ty, [$idx:expr, $($tail:expr),+]) => {
+        (<$type>::from(PRIMES[$idx]) * composite!($type, [$($tail),+]))
+    };
+    ($type:ty, [$idx:expr]) => {
+        <$type>::from(PRIMES[$idx])
+    };
+}
+
+#[allow(unused)]
+fn composite<T: From<u64> + Ops + OpsFrom>(arr: &[u64]) -> T {
+    arr.iter().fold(T::from(1), |acc, &x| T::from(acc * T::from(x)))
+}
+
 macro_rules! impl_case {
-    ($fn:ident, $name:literal, $group:literal, $signed:ty, $unsigned:ty, [$i0:expr, $i1:expr, $i2:expr, $i3:expr], [$op:tt]) => {
+    ($group:expr, $type:ty, $name:expr, [$($idx1:expr),+], [$($idx2:expr),+], [$op:tt]) => {
+        let val1 = &composite!($type, [$($idx1),+]);
+        let val2 = &composite!($type, [$($idx2),+]);
+
+        $group.bench_function(format!("{} {}", stringify!($type), $name), |b| b.iter_with_large_drop(|| black_box(val1 $op val2)));
+    };
+    ($fn:ident, $name:literal, $group:literal, [$($type:ty: [$($idx1:expr),+], [$($idx2:expr),+]),+], [$op:tt]) => {
         fn $fn(c: &mut Criterion) {
             let mut group = c.benchmark_group($group);
 
             group.sample_size(256);
 
-            let signed_a = &(<$signed>::from(PRIMES[$i0]) * <$signed>::from(PRIMES[$i1]));
-            let signed_b = &(<$signed>::from(PRIMES[$i2]) * <$signed>::from(PRIMES[$i3]));
-            let unsigned_a = &(<$unsigned>::from(PRIMES[$i0]) * <$unsigned>::from(PRIMES[$i1]));
-            let unsigned_b = &(<$unsigned>::from(PRIMES[$i2]) * <$unsigned>::from(PRIMES[$i3]));
-
-            group.bench_function(format!("Signed {}", $name), |b| b.iter_with_large_drop(|| black_box(signed_a $op signed_b)));
-            group.bench_function(format!("Unsigned {}", $name), |b| b.iter_with_large_drop(|| black_box(unsigned_a $op unsigned_b)));
+            $(impl_case!(group, $type, $name, [$($idx1),+], [$($idx2),+], [$op]);)+
         }
     };
 }
 
-impl_case!(add_long, "Add", "Longs", SignedLong, UnsignedLong, [0, 1, 2, 3], [+]);
-impl_case!(sub_long, "Sub", "Longs", SignedLong, UnsignedLong, [0, 1, 2, 3], [-]);
-impl_case!(mul_long, "Mul", "Longs", SignedLong, UnsignedLong, [0, 1, 2, 3], [*]);
-impl_case!(div_long, "Div", "Longs", SignedLong, UnsignedLong, [0, 1, 4, 4], [/]);
+impl_case!(add,     "Add", "Num", [SignedLong: [0, 1], [2, 3], UnsignedLong: [0, 1], [2, 3], S64: [0, 1], [2, 3], U64: [0, 1], [2, 3]], [+]);
+impl_case!(sub,     "Sub", "Num", [SignedLong: [0, 1], [2, 3], UnsignedLong: [0, 1], [2, 3], S64: [0, 1], [2, 3], U64: [0, 1], [2, 3]], [-]);
+impl_case!(mul,     "Mul", "Num", [SignedLong: [0, 1], [2, 3], UnsignedLong: [0, 1], [2, 3], S64: [0, 1], [2, 3], U64: [0, 1], [2, 3]], [*]);
+impl_case!(div,     "Div", "Num", [SignedLong: [0, 1], [4, 4], UnsignedLong: [0, 1], [4, 4], S64: [0, 1], [4, 4], U64: [0, 1], [4, 4]], [/]);
+impl_case!(bitor,   "Or",  "Num", [SignedLong: [0, 1], [2, 3], UnsignedLong: [0, 1], [2, 3], S64: [0, 1], [2, 3], U64: [0, 1], [2, 3]], [|]);
+impl_case!(bitand,  "And", "Num", [SignedLong: [0, 1], [2, 3], UnsignedLong: [0, 1], [2, 3], S64: [0, 1], [2, 3], U64: [0, 1], [2, 3]], [&]);
+impl_case!(bitxor,  "Xor", "Num", [SignedLong: [0, 1], [2, 3], UnsignedLong: [0, 1], [2, 3], S64: [0, 1], [2, 3], U64: [0, 1], [2, 3]], [^]);
 
-impl_case!(add_fixed, "Add", "Fixed", S64, U64, [0, 1, 2, 3], [+]);
-impl_case!(sub_fixed, "Sub", "Fixed", S64, U64, [0, 1, 2, 3], [-]);
-impl_case!(mul_fixed, "Mul", "Fixed", S64, U64, [0, 1, 2, 3], [*]);
-impl_case!(div_fixed, "Div", "Fixed", S64, U64, [0, 1, 4, 4], [/]);
-
-criterion_group!(group, add_long, add_fixed, sub_long, sub_fixed, mul_long, mul_fixed, div_long, div_fixed);
+criterion_group!(group, add, sub, mul, div, bitor, bitand, bitxor,);
 
 criterion_main!(group);
