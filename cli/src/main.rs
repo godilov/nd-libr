2eba3824d40f7936b840202e59d8f1bb0d1453f9
German Odilov <godilov@tuta.io>
2026-01-14 09:52:08 +0500

    fix(cli): compilation fix

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,7 +2,7 @@ use std::{fs::File, path::PathBuf};
 
 use anyhow::Error;
 use clap::{Parser, Subcommand};
-use ndlib::num::{Num, prime::Primes};
+use ndlib::num::prime::{PrimalityExtension, Primes};
 
 #[derive(Parser)]
 #[command(version, about, long_about)]
