0a40d2e25499a62df6a712d646060640a882f8fa
German Odilov <godilov@tuta.io>
2026-03-05 06:29:24 +0500

    impr(num): wrapping/saturating/unbounded num and ext

--- lib/num/src/lib.rs
+++ lib/num/src/lib.rs
@@ -565,17 +565,23 @@ pub mod prime {
 #[ndfwd::std(self.0 with N)]
 #[ndfwd::cmp(self.0 with N)]
 #[ndfwd::fmt(self.0 with N)]
-pub struct Wrapping<N: Num>(pub N);
+#[ndfwd::def(self.0 with N: Num)]
+#[ndfwd::def(self.0 with N: NumExt)]
+pub struct Wrapping<N: Num + NumExt>(pub N);
 
 #[ndfwd::std(self.0 with N)]
 #[ndfwd::cmp(self.0 with N)]
 #[ndfwd::fmt(self.0 with N)]
-pub struct Saturating<N: Num>(pub N);
+#[ndfwd::def(self.0 with N: Num)]
+#[ndfwd::def(self.0 with N: NumExt)]
+pub struct Saturating<N: Num + NumExt>(pub N);
 
 #[ndfwd::std(self.0 with N)]
 #[ndfwd::cmp(self.0 with N)]
 #[ndfwd::fmt(self.0 with N)]
-pub struct Unbounded<N: Num>(pub N);
+#[ndfwd::def(self.0 with N: Num)]
+#[ndfwd::def(self.0 with N: NumExt)]
+pub struct Unbounded<N: Num + NumExt>(pub N);
 
 #[ndfwd::std(self.0 with N)]
 #[ndfwd::cmp(self.0 with N)]
@@ -963,6 +969,24 @@ sign_from!(@signed [i8, i16, i32, i64, i128, isize]);
 sign_from!(@unsigned [u8, u16, u32, u64, u128, usize]);
 
 ndops::all! { @stdbin (lhs: Sign, rhs: Sign) -> Sign, [* (lhs as i8) * (rhs as i8)] }
+
+impl<N: Num + NumExt> From<N> for Wrapping<N> {
+    fn from(value: N) -> Self {
+        Wrapping(value)
+    }
+}
+
+impl<N: Num + NumExt> From<N> for Saturating<N> {
+    fn from(value: N) -> Self {
+        Saturating(value)
+    }
+}
+
+impl<N: Num + NumExt> From<N> for Unbounded<N> {
+    fn from(value: N) -> Self {
+        Unbounded(value)
+    }
+}
 
 impl<N: Num + NumExt + NumUnsigned + Binary, const BITS: usize> From<N> for Width<N, BITS> {
     fn from(value: N) -> Self {
