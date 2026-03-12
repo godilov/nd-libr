1a502a6ba8158bb915c82119b3b62c06cf81ae2c
German Odilov <godilov@tuta.io>
2026-03-12 23:05:05 +0500

    docs(libr): inline

--- lib/libr/src/lib.rs
+++ lib/libr/src/lib.rs
@@ -1,8 +1,11 @@
 #![doc = include_str!("../README.md")]
 
 #[cfg(feature = "core")]
+#[doc(inline)]
 pub use ndcore::{convert, iter, ops};
 #[cfg(feature = "mem")]
+#[doc(inline)]
 pub use ndmem as mem;
 #[cfg(feature = "num")]
+#[doc(inline)]
 pub use ndnum as num;
