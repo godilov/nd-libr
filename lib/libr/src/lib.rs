d6788120e4278fcc5ef4248d1270585aa1bff1c5
German Odilov <godilov@tuta.io>
2026-03-17 19:05:24 +0500

    fix(libr): feature-flag

--- lib/libr/src/lib.rs
+++ lib/libr/src/lib.rs
@@ -1,11 +1,11 @@
 #![doc = include_str!("../README.md")]
 
-#[cfg(feature = "core")]
-#[doc(inline)]
-pub use ndext as ext;
 #[cfg(feature = "crypto")]
 #[doc(inline)]
 pub use ndcrypto as crypto;
+#[cfg(feature = "ext")]
+#[doc(inline)]
+pub use ndext as ext;
 #[cfg(feature = "mem")]
 #[doc(inline)]
 pub use ndmem as mem;
