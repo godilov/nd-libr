7f40e92627ea6c3fc91fad16254936ea0b603db4
German Odilov <godilov@tuta.io>
2026-09-01 15:15:06 +0500

    ref(bench): 4.5 + 1.5

--- lib/num/benches/long.rs
+++ lib/num/benches/long.rs
@@ -49,8 +49,8 @@ macro_rules! state {
     ($criterion:expr, $group:literal) => {{
         let mut group = $criterion.benchmark_group($group);
 
-        group.measurement_time(Duration::from_secs(10));
-        group.warm_up_time(Duration::from_secs(5));
+        group.measurement_time(Duration::from_millis(4500));
+        group.warm_up_time(Duration::from_millis(1500));
 
         (group, StdRng::seed_from_u64(PRIMES[0] * PRIMES[1]))
     }};
